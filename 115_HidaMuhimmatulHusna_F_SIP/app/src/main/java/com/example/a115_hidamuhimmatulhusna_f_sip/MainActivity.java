16907024f03a7528994d8b116690d74e9560d12d
hidaMH <hidamuhimmah@gmail.com>
2020-09-29 15:58:25 +0700

    Task SIP

---
+++ 115_HidaMuhimmatulHusna_F_SIP/app/src/main/java/com/example/a115_hidamuhimmatulhusna_f_sip/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a115_hidamuhimmatulhusna_f_sip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
